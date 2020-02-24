3b910564964fddaa9e34763468769f39a2673406
cbsalt <60279686+cbsalt@users.noreply.github.com>
2020-02-24 11:33:49 -0300

    Upload GoBarber

---
+++ src/app/controllers/ScheduleController.js
@@ -0,0 +1,42 @@
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
+import { Op } from 'sequelize';
+
+import User from '../models/User';
+import Appointment from '../models/Appointment';
+
+class ScheduleController {
+  async index(req, res) {
+    const checkUserProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!checkUserProvider) {
+      return res.status(401).json({ erro: 'User is not a provider' });
+    }
+
+    const { date } = req.query;
+    const parseDate = parseISO(date);
+
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(parseDate), endOfDay(parseDate)],
+        },
+      },
+      include: [
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
+      ],
+      order: ['date'],
+    });
+
+    return res.json({ appointments });
+  }
+}
+
+export default new ScheduleController();
